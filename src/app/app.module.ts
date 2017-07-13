f2531590b31428d16870b0c7f1beaa860db63e66
Sudarshan-Cognizant <sudarson.r@cognizant.com>
2017-07-13 19:35:30 +0530

    Demo App Started with Architecture

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
-
+import {RouterModule,Routes} from '@angular/router';
 import { AppComponent } from './app.component';
 import { TestComponent } from './test.component';
 import { Hero } from './test.component';
+import {HeroDetailComponent} from './hero-detail.component';
 
+const appRoutes:Routes =[
+{path:'Home',component:AppComponent},
+{path:'hero-list' , component : TestComponent},
+{path:'hero-details', component:TestComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeroDetailComponent,
     TestComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
-  bootstrap: [TestComponent ,AppComponent]
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/hero-detail.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+
+
+@Component({
+	template:'<h1>Hero Details Page</h1> '
+
+})
+
+export class HeroDetailComponent{
+
+}
